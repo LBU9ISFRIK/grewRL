482e02e5dfec330438e4a0ae42af52a147abdf3b
ParkJunBeom603 <tokomon320@gamecrewlab.com>
2020-08-14 19:08:48 +0900

    pybullet

--- bullet3-master/examples/pybullet/gym/pybullet_envs/gym_locomotion_envs.py
+++ bullet3-master/examples/pybullet/gym/pybullet_envs/gym_locomotion_envs.py
@@ -4,6 +4,7 @@ import numpy as np
 import pybullet
 from robot_locomotors import Hopper, Walker2D, HalfCheetah, Ant, Humanoid, HumanoidFlagrun, HumanoidFlagrunHarder
 
+import time
 
 class WalkerBaseBulletEnv(MJCFBaseBulletEnv):
 
@@ -61,6 +62,7 @@ class WalkerBaseBulletEnv(MJCFBaseBulletEnv):
   joints_at_limit_cost = -0.1  # discourage stuck joints
 
   def step(self, a):
+    #time.sleep(0.5)
     if not self.scene.multiplayer:  # if multiplayer, action first applied to all robots, then global step() called, then _step() for all robots with the same actions
       self.robot.apply_action(a)
       self.scene.global_step()
@@ -69,7 +71,7 @@ class WalkerBaseBulletEnv(MJCFBaseBulletEnv):
 
     self._alive = float(
         self.robot.alive_bonus(
-            state[0] + self.robot.initial_z,
+            state[0] + self.robot.initial_z, #현재 높이. (state[0] == 현재 높이(z) - 초기 높이)이므로 (state[0] + 초기 높이 == 현재 높이)가 된다.
             self.robot.body_rpy[1]))  # state[0] is body height above ground, body_rpy[1] is pitch
     done = self._isDone()
     if not np.isfinite(state).all():
@@ -93,32 +95,24 @@ class WalkerBaseBulletEnv(MJCFBaseBulletEnv):
       else:
         self.robot.feet_contact[i] = 0.0
 
-    electricity_cost = self.electricity_cost * float(np.abs(a * self.robot.joint_speeds).mean(
-    ))  # let's assume we have DC motor with controller, and reverse current braking
+    electricity_cost = self.electricity_cost * float(np.abs(a * self.robot.joint_speeds).mean())  # let's assume we have DC motor with controller, and reverse current braking
     electricity_cost += self.stall_torque_cost * float(np.square(a).mean())
 
     joints_at_limit_cost = float(self.joints_at_limit_cost * self.robot.joints_at_limit)
     debugmode = 0
     if (debugmode):
-      print("alive=")
-      print(self._alive)
-      print("progress")
-      print(progress)
-      print("electricity_cost")
-      print(electricity_cost)
-      print("joints_at_limit_cost")
-      print(joints_at_limit_cost)
-      print("feet_collision_cost")
-      print(feet_collision_cost)
+      print("alive=",self._alive)
+      print("progress=",progress)
+      print("electricity_cost=",electricity_cost)
+      print("joints_at_limit_cost=",joints_at_limit_cost)
+      print("feet_collision_cost=",feet_collision_cost)
 
     self.rewards = [
         self._alive, progress, electricity_cost, joints_at_limit_cost, feet_collision_cost
     ]
-    if (debugmode):
-      print("rewards=")
-      print(self.rewards)
-      print("sum rewards")
-      print(sum(self.rewards))
+    #if (debugmode):
+    #  print("rewards=",self.rewards)
+    #  print("sum rewards=",sum(self.rewards))
     self.HUD(state, a, done)
     self.reward += sum(self.rewards)
 
