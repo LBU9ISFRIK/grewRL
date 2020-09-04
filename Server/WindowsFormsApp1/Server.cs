18cccc5bdd6a7a1c02f76f2902be49eaa4346950
ParkJunBeom603 <tokomon320@gamecrewlab.com>
2020-09-04 18:59:07 +0900

    server

---
+++ Server/WindowsFormsApp1/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApp1
+{
+    public partial class Server : Form
+    {
+        string strIP = "127.0.0.1";
+        int port = 8000;
+
+        UdpClient udpServer;
+        IPEndPoint remoteEP;
+
+        byte[] data;
+
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            udpServer = new UdpClient(port);
+            remoteEP = new IPEndPoint(IPAddress.Parse(strIP), port);
+
+            //데이터 받기
+            //int length = socket.Receive(rBuffer, 0, rBuffer.Length, SocketFlags.None);
+
+            //디코딩
+            //string result = Encoding.UTF8.GetString(rBuffer);
+
+            //label1.Text = "전송된 데이터 : ";
+            //label1.Text += result + "\n";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int width = 10;
+            //string send = "leg_count : " + send_textBox.Text;
+            //send += "$width : " + width;
+
+            data = Encoding.Default.GetBytes(send_textBox.Text);
+            udpServer.Send(data, data.Length, remoteEP);
+        }
+
+        private void Server_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            udpServer.Close();
+        }
+    }
+}
