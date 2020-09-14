08647cdae577eada3581a63e0bb2822759537c06
aavuzbcrew <69282906+aavuzbcrew@users.noreply.github.com>
2020-09-14 09:43:19 +0900

    Csong

---
+++ Deep_RL/CSONG/Client/Assets/Resources/Scripts/Client.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+    public Terrain terrain;
+    public int sizeX;
+    public int sizeY;
+    public int sizeZ;
+    public Vector3 terrainSize;
+    public GameObject newObject;
+    
+
+    public struct UdpState
+    {
+        public UdpClient udpClient;
+        public IPEndPoint remoteEP;
+    }
+
+    string strIP = "127.0.0.1";
+    int port = 8000;
+
+    UdpClient udpClient;
+    IPEndPoint remoteEP;
+    UdpState udpState = new UdpState();
+
+    byte[] data;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remoteEP = new IPEndPoint(IPAddress.Parse(strIP), port);
+        udpClient = new UdpClient(remoteEP);
+        udpClient.Client.Blocking = false;
+        udpClient.Client.ReceiveTimeout = 1000;
+
+        udpState.udpClient = udpClient;
+        udpState.remoteEP = remoteEP;
+
+        //udpClient.Client.Bind(remoteEP);
+
+        //udpClient.BeginReceive(new System.AsyncCallback(ReceiveCallback), udpState); //한번만 받음(비동기)
+
+        while (udpClient.Available > 0)
+        {
+            // receive bytes
+            data = udpClient.Receive(ref remoteEP);
+            print("clear data : " + Encoding.Default.GetString(data));
+        }
+
+        StartCoroutine(ReceiveData());
+
+
+        terrain = GetComponent<Terrain>();
+
+       
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //data = udpClient.Receive(ref remoteEP); //받을때까지 멈춤(동기)
+        //print(string.Format("{0} : 수신 : {1}", remoteEP.ToString(), Encoding.Default.GetString(data)));
+    }
+
+    private void OnDestroy()
+    {
+        udpClient.Close();
+    }
+
+    IEnumerator ReceiveData()
+    {
+        while (true)
+        {
+            try
+            {
+                if (udpClient.Available > 0)
+                {
+                    // receive bytes
+                    data = udpClient.Receive(ref remoteEP);
+                    print(Encoding.Default.GetString(data));
+                    string str = Encoding.Default.GetString(data);
+
+                    string[] sp = str.Split('$');
+
+
+                    Vector3 temp = terrain.transform.localScale;
+
+                    print(sp[0]);
+                    print(sp[1]);
+
+                    //Ground W
+                    temp.x = int.Parse(sp[0]);
+                    //Ground H
+                    temp.z = int.Parse(sp[1]);
+                    terrain.transform.localScale = temp;
+
+                    // tile size X = sp[2]
+                    int sizeX = int.Parse(sp[2]);
+                    int sizeY = 600;
+                    // tile size Y = sp[3]
+                    int sizeZ = int.Parse(sp[3]);
+
+
+                    terrainSize = new Vector3(sizeX, sizeY, sizeZ);
+                    terrain.terrainData.size = terrainSize;
+
+                    int num = int.Parse(sp[4]);
+
+                    if (num > 0)
+                    {
+                        
+
+                        for (int j = 1; j <= num; j++)
+                        {
+                            newObject =  Instantiate(newObject, terrain.transform.localScale, terrain.transform.rotation);
+                        }
+
+
+
+                    }
+
+
+                }
+            }
+            catch (System.Exception err)
+            {
+                //print(err.ToString());
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Deep_RL/CSONG/Client/Assets/Resources/Scripts/ObjectCreator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCreator : MonoBehaviour
+{
+
+    GameObject newObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        newObject = (GameObject)Resources.Load("Prefabs/Wolf", typeof(GameObject));
+        newObject.AddComponent<Rigidbody>();
+        newObject.AddComponent<BoxCollider>();
+        newObject.transform.position = new Vector3(4f, 0.5f, 4f);
+
+
+        Instantiate(newObject);
+
+       
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            /*GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(4f, 0.5f, 7f);
+            //cube.AddComponent<Light>();*/
+
+                    }
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.position = new Vector3(6f, 3f, 2f);
+            sphere.AddComponent<Camera>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject sphere2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere2.transform.position = new Vector3(1f, 3f, 2f);
+            sphere2.AddComponent<Rigidbody>();
+            
+        }
+
+        
+
+
+
+    }
+}
